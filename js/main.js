2a47085a528b689a62fe5088cd974f4168eaf69c
Roman <saldanroman@gmail.com>
2019-02-27 17:04:19 +0000

    Created JS

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const greetings = 'Hello World!';
+alert(greetings);
+
+// number
+// string
+// boolean
+// null
+// undefined
+// Object
+// Symbol
+
+
+const age = 42;
+let isMarried = false;
+
+const userAge = parseFloat(prompt('Enter your age'));
+alert('Your age is ' + userAge);
+const nextAge = userAge + 1;
+alert('Next year you will be ' + nextAge);
+
+/*const ageString = checkAge(userAge);
+alert('You are ' + ageString);
+
+function checkAge(age) {
+    if (age < 18) { // < > <= >= == === != !==
+        return 'child';
+    } else {
+        return 'adult';
+    }
+} */
+
+// Task for you 
+// Modify function to show 'old' if you user is older than 100 
+
+
+const ageString = checkAge(userAge);
+alert('You are ' + ageString);
+
+function checkAge(age) {
+    if (age >= 100) { // < > <= >= == === != !==
+        return 'old';
+    } 
+    else{ 
+        if(age >= 18){
+        return 'adult';
+    }
+     else{   return 'cild';
+     }
+    }
+}
